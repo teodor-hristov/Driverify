f7d0f9a8beb08f91b69e68e51aa6b88b6b4e541a
Teodor Hristov <26335078+teodtedo771@users.noreply.github.com>
2019-12-30 00:13:53 +0200

    Hide sleep detector

--- app/src/main/java/com/demotxt/droidsrce/homedashboard/LoginActivity.java
+++ app/src/main/java/com/demotxt/droidsrce/homedashboard/LoginActivity.java
@@ -8,7 +8,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class LoginActivity extends AppCompatActivity {
-    private static int SPLASH_TIME_OUT = 1500;
+    private static int SPLASH_TIME_OUT = 1000;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
