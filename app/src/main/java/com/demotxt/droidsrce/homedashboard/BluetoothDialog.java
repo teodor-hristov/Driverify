935151b2d5910c16140e7c838bed7af76bc2a9da
Teodor Hristov <26335078+teodtedo771@users.noreply.github.com>
2020-01-05 14:57:40 +0200

    Connection with bluetooth devices successful but i need to move that connection in non ui thread because of performance

--- app/src/main/java/com/demotxt/droidsrce/homedashboard/BluetoothDialog.java
+++ app/src/main/java/com/demotxt/droidsrce/homedashboard/BluetoothDialog.java
@@ -3,15 +3,30 @@ package com.demotxt.droidsrce.homedashboard;
 import android.app.Dialog;
 import android.bluetooth.BluetoothAdapter;
 import android.bluetooth.BluetoothDevice;
+import android.bluetooth.BluetoothSocket;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.ParcelUuid;
+import android.util.Log;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatDialogFragment;
+import androidx.versionedparcelable.ParcelUtils;
 
+import com.github.pires.obd.commands.protocol.EchoOffCommand;
+import com.github.pires.obd.commands.protocol.LineFeedOffCommand;
+import com.github.pires.obd.commands.protocol.SelectProtocolCommand;
+import com.github.pires.obd.commands.protocol.TimeoutCommand;
+import com.github.pires.obd.commands.temperature.AmbientAirTemperatureCommand;
+import com.github.pires.obd.enums.ObdProtocols;
+
+import java.io.IOException;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
 import java.util.Set;
+import java.util.UUID;
 
 public class BluetoothDialog extends AppCompatDialogFragment {
     private static final String TAG = "BluetoothDialog";
@@ -19,6 +34,10 @@ public class BluetoothDialog extends AppCompatDialogFragment {
     private Set<BluetoothDevice> pairedDev;
     private String[] devices;
     private AlertDialog.Builder builder;
+
+    private BluetoothDevice selectedDevice;
+    private UUID deviceAdress;
+    BluetoothSocket socket;
 
     @Override
     public Dialog onCreateDialog(Bundle savedInstanceState) {
@@ -37,7 +56,42 @@ public class BluetoothDialog extends AppCompatDialogFragment {
                 builder.setTitle(R.string.dialogHead)
                         .setItems(devices, new DialogInterface.OnClickListener() {
                             public void onClick(DialogInterface dialog, int which) {
+                                selectedDevice = (pairedDev.toArray(new BluetoothDevice[pairedDev.size()]))[which];
+                                try {
+                                    deviceAdress = getUUID(bluetoothAdapter, which);
+                                    if(deviceAdress == null)
+                                        throw new Exception("Null");
+                                } catch (NoSuchMethodException e) {
+                                    Log.e(TAG, e.toString());
+                                } catch (InvocationTargetException e) {
+                                    Log.e(TAG, e.toString());
+                                } catch (IllegalAccessException e) {
+                                    Log.e(TAG, e.toString());
+                                } catch (Exception e) {
+                                    e.printStackTrace();
+                                }
+                                bluetoothAdapter.getRemoteDevice(selectedDevice.getAddress());
+                                try {
+                                    socket = selectedDevice.createInsecureRfcommSocketToServiceRecord(deviceAdress);
+                                } catch (IOException e) {
+                                    Log.e(TAG, e.toString());
+                                }
 
+                                try {
+                                    socket.connect();
+                                } catch (IOException e) {
+                                    Log.e(TAG, e.toString());
+                                }
+                                // execute commands
+                                try {
+                                    new EchoOffCommand().run(socket.getInputStream(), socket.getOutputStream());
+                                    new LineFeedOffCommand().run(socket.getInputStream(), socket.getOutputStream());
+                                    new TimeoutCommand(125).run(socket.getInputStream(), socket.getOutputStream());
+                                    new SelectProtocolCommand(ObdProtocols.AUTO).run(socket.getInputStream(), socket.getOutputStream());
+                                    new AmbientAirTemperatureCommand().run(socket.getInputStream(), socket.getOutputStream());
+                                } catch (Exception e) {
+                                    // handle errors
+                                }
                             }
                         });
             }else{
@@ -61,6 +115,21 @@ public class BluetoothDialog extends AppCompatDialogFragment {
                 return devices;
             }
         return null;
+    }
+    private UUID getUUID(BluetoothAdapter adapter, int index) throws NoSuchMethodException, InvocationTargetException, IllegalAccessException {
+        Method getUuidsMethod = BluetoothAdapter.class.getDeclaredMethod("getUuids", null);
+        int i = 0;
+        UUID uuid = null;
+        ParcelUuid[] uuids = (ParcelUuid[]) getUuidsMethod.invoke(adapter, null);
+
+        for (ParcelUuid item: uuids) {
+            if(i == index) {
+                uuid = item.getUuid();
+                break;
+            }
+            i++;
+        }
+        return uuid;
     }
 
 
