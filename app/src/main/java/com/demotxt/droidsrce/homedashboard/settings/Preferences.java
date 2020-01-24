4697e90500b8946c4b16a59196d25f615f036e22
Teodor Hristov <26335078+teodtedo771@users.noreply.github.com>
2020-01-24 23:21:34 +0200

    Add realtime bt on/off

--- app/src/main/java/com/demotxt/droidsrce/homedashboard/settings/Preferences.java
+++ app/src/main/java/com/demotxt/droidsrce/homedashboard/settings/Preferences.java
@@ -18,8 +18,8 @@ import java.util.ArrayList;
 import java.util.Set;
 
 public class Preferences extends PreferenceActivity implements Preference.OnPreferenceChangeListener {
-    
 
+    private static final String TAG = "Preferences";
     public static final String BLUETOOTH_LIST_KEY = "bluetooth_list_preference";
     public static final String BLUETOOTH_ENABLE = "enable_bluetooth_preference";
 
@@ -29,6 +29,8 @@ public class Preferences extends PreferenceActivity implements Preference.OnPref
     private Set<BluetoothDevice> pairedDevices;
     private ListPreference listBtDevices;
     private Preference mPreferenceCheckBt;
+    private ArrayList<CharSequence> pairedDeviceStrings;
+    private ArrayList<CharSequence> vals;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -40,81 +42,110 @@ public class Preferences extends PreferenceActivity implements Preference.OnPref
         /*
          * Read preferences resources available at res/xml/preferences.xml
          */
-        ArrayList<CharSequence> pairedDeviceStrings = new ArrayList<>();
-        ArrayList<CharSequence> vals = new ArrayList<>();
-        listBtDevices= (ListPreference) getPreferenceScreen()
-                .findPreference(BLUETOOTH_LIST_KEY);
+         pairedDeviceStrings = new ArrayList<>();
+         vals = new ArrayList<>();
+         listBtDevices= (ListPreference) getPreferenceScreen().findPreference(BLUETOOTH_LIST_KEY);
+         listBtDevices.setEntries(pairedDeviceStrings.toArray(new CharSequence[0]));
+         listBtDevices.setEntryValues(vals.toArray(new CharSequence[0]));
+
 
         /*
          * Select device to connect
          */
         if (mBtAdapter == null) {
-            listBtDevices
-                    .setEntries(pairedDeviceStrings.toArray(new CharSequence[0]));
+            listBtDevices.setEntries(pairedDeviceStrings.toArray(new CharSequence[0]));
             listBtDevices.setEntryValues(vals.toArray(new CharSequence[0]));
-
             // we shouldn't get here, still warn user
-            Toast.makeText(this, "This device does not support Bluetooth.",
-                    Toast.LENGTH_LONG).show();
-
+            makeToast("This device does not support Bluetooth.");
             return;
         }
-        /*
-         * Listen for preferences click.
-         *
-         * TODO there are so many repeated validations :-/
-         */
-
-        listBtDevices.setEntries(new CharSequence[1]);
-        listBtDevices.setEntryValues(new CharSequence[1]);
-        listBtDevices.setOnPreferenceClickListener(new Preference.OnPreferenceClickListener() {
-            public boolean onPreferenceClick(Preference preference) {
-                // see what I mean in the previous comment?
-                if (mBtAdapter == null || !mBtAdapter.isEnabled()) {
-                    Toast.makeText(thisActivity,
-                            "This device does not support Bluetooth or it is disabled.",
-                            Toast.LENGTH_LONG).show();
-                    return false;
-                }
-                return true;
-            }
-        });
+//        /*
+//         * Listen for preferences click.
+//         *
+//         * TODO there are so many repeated validations :-/
+//         */
+//        listBtDevices.setEntries(new CharSequence[1]);
+//        listBtDevices.setEntryValues(new CharSequence[1]);
+//        listBtDevices.setOnPreferenceClickListener(new Preference.OnPreferenceClickListener() {
+//            public boolean onPreferenceClick(Preference preference) {
+//                // see what I mean in the previous comment?
+//                if (mBtAdapter == null || !mBtAdapter.isEnabled()) {
+//                    makeToast("This device does not support Bluetooth or it is disabled.");
+//                    return false;
+//                }
+//                return true;
+//            }
+//        });
 
         /*
          * Get paired devices and populate preference list.
+         * TODO: Double click problem
          */
-        pairedDevices = mBtAdapter.getBondedDevices();
-        if (pairedDevices.size() > 0) {
-            for (BluetoothDevice device : pairedDevices) {
-                pairedDeviceStrings.add(device.getName() + "\n" + device.getAddress());
-                vals.add(device.getAddress());
-            }
-        }
-        listBtDevices.setEntries(pairedDeviceStrings.toArray(new CharSequence[0]));
-        listBtDevices.setEntryValues(vals.toArray(new CharSequence[0]));
-
-        /*
-         * Check if bt enable/disable state is changed
-         */
-        if(mBtAdapter != null){
-            mPreferenceCheckBt.setOnPreferenceClickListener(clickListener);
-        }
-
-    }
-        Preference.OnPreferenceClickListener clickListener = new Preference.OnPreferenceClickListener() {
+        listBtDevices.setOnPreferenceClickListener(new Preference.OnPreferenceClickListener() {
             @Override
             public boolean onPreferenceClick(Preference preference) {
-                if(preference.getKey().equals(BLUETOOTH_ENABLE)){
-                    if(preference.isEnabled()){
-                         mBtAdapter.enable();
-                        Log.i(TAG, "onPreferenceClick: ");
-                    }else{
-                         mBtAdapter.disable();
+                if(mBtAdapter.isEnabled() && mBtAdapter != null){
+                    //Clear old devices and setting it clear
+                    if(vals != null && pairedDeviceStrings != null) {
+                        vals.clear();
+                        pairedDeviceStrings.clear();
+                        listBtDevices.setEntries(pairedDeviceStrings.toArray(new CharSequence[0]));
+                        listBtDevices.setEntryValues(vals.toArray(new CharSequence[0]));
+                    }
+
+                    pairedDevices = mBtAdapter.getBondedDevices();
+                    if (pairedDevices.size() > 0) {
+                        for (BluetoothDevice device : pairedDevices) {
+                            pairedDeviceStrings.add(device.getName() + "\n" + device.getAddress());
+                            vals.add(device.getAddress());
+                        }
+                    }
+                    listBtDevices.setEntries(pairedDeviceStrings.toArray(new CharSequence[0]));
+                    listBtDevices.setEntryValues(vals.toArray(new CharSequence[0]));
+                }else{
+                    //Clear old devices and setting it clear
+                    if(vals != null && pairedDeviceStrings != null) {
+                        vals.clear();
+                        pairedDeviceStrings.clear();
+                        listBtDevices.setEntries(pairedDeviceStrings.toArray(new CharSequence[0]));
+                        listBtDevices.setEntryValues(vals.toArray(new CharSequence[0]));
                     }
                 }
                 return false;
             }
-        };
+        });
+
+        /*
+         * Enable/Disable bt realtime
+         */
+        if(mBtAdapter != null){
+            mPreferenceCheckBt.setOnPreferenceClickListener(new Preference.OnPreferenceClickListener() {
+                @Override
+                public boolean onPreferenceClick(Preference preference) {
+                    if(preference.getKey().equals(BLUETOOTH_ENABLE)){
+                        if(!mBtAdapter.isEnabled()){
+                            if(mBtAdapter.enable()) {
+                                Log.i(TAG, "onPreferenceClick: enable");
+                                makeToast("Bluetooth is enabled!");
+                            }else{
+                                makeToast("There was an error.");
+                            }
+
+                        }else{
+                            if(mBtAdapter.disable()) {
+                                Log.i(TAG, "onPreferenceClick: disable");
+                                makeToast("Bluetooth is disabled!");
+                            }else{
+                                makeToast("There was an error.");
+                            }
+                        }
+                    }
+                    return false;
+                }
+            });
+        }
+
+    }
 
     @Override
     public boolean onPreferenceChange(Preference preference, Object o) {
@@ -124,5 +155,8 @@ public class Preferences extends PreferenceActivity implements Preference.OnPref
     @Override
     public void onPointerCaptureChanged(boolean hasCapture) {
 
+    }
+    public void makeToast(String txt){
+        Toast.makeText(thisActivity, txt, Toast.LENGTH_SHORT).show();
     }
 }
