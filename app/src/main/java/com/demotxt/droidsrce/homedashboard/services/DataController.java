c076bf39c224db16ed6affb00b2375ad7b64c616
Teodor Hristov <26335078+teodtedo771@users.noreply.github.com>
2020-02-14 15:41:24 +0200

    TODO: all important actions

--- app/src/main/java/com/demotxt/droidsrce/homedashboard/services/DataController.java
+++ app/src/main/java/com/demotxt/droidsrce/homedashboard/services/DataController.java
@@ -1,6 +1,31 @@
 package com.demotxt.droidsrce.homedashboard.services;
 
 import android.app.Service;
+import android.content.Intent;
+import android.os.IBinder;
+
+import androidx.annotation.Nullable;
 
 public class DataController extends Service {
+    @Nullable
+    @Override
+    public IBinder onBind(Intent intent) {
+        return null;
+    }
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        /**
+         * TODO: Create files, write in files...
+         */
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        /**
+         * TODO: Close all writers, save all states, break connections
+         */
+    }
 }
