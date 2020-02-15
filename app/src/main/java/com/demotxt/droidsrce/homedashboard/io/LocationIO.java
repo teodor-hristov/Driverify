a444248277393ff679ffeebf5bddbb436d454bcc
Teodor Hristov <26335078+teodtedo771@users.noreply.github.com>
2020-02-15 18:02:54 +0200

    Import fix

--- app/src/main/java/com/demotxt/droidsrce/homedashboard/io/LocationIO.java
+++ app/src/main/java/com/demotxt/droidsrce/homedashboard/io/LocationIO.java
@@ -1,9 +1,7 @@
 package com.demotxt.droidsrce.homedashboard.io;
 
 import android.Manifest;
-import android.app.AlertDialog;
 import android.content.Context;
-import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.location.Location;
