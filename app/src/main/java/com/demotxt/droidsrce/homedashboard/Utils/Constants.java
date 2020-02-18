a80038f90684c59b027217898e416d0298c7a455
Teodor Hristov <26335078+teodtedo771@users.noreply.github.com>
2020-02-18 21:44:30 +0200

    capital letters all constants

--- app/src/main/java/com/demotxt/droidsrce/homedashboard/Utils/Constants.java
+++ app/src/main/java/com/demotxt/droidsrce/homedashboard/Utils/Constants.java
@@ -1,18 +1,18 @@
 package com.demotxt.droidsrce.homedashboard.Utils;
 
 public class Constants {
-    public static final String connected = "BLUETOOTH_CONNECTED";
-    public static final String disconnected = "BLUETOOTH_DISCONNECTED";
-    public static final String receiveData = "OBD_DATA_RECEIVE";
-    public static final String extra = "INTENT_EXTRA_DATA";
+    public static final String CONNECTED = "BLUETOOTH_CONNECTED";
+    public static final String DISCONNECTED = "BLUETOOTH_DISCONNECTED";
+    public static final String RECEIVE_DATA = "OBD_DATA_RECEIVE";
+    public static final String EXTRA = "INTENT_EXTRA_DATA";
     public static final String DTC = "INTENT_TROUBLE_CODES";
-    public static final String GPSEnabled = "GPS_ENABLE";
-    public static final String GPSDisabled = "GPS_DISABLE";
-    public static final String GPSLiveData = "GPS_LIVE_DATA";
-    public static final String GPSPutExtra = "coordinates";
-    public static final String DataLogPath = "storage/emulated/0/Driverify/Logs/";
-    public static final String timeIntervalKey = "obd_update_period_preference";
+    public static final String GPS_ENABLED = "GPS_ENABLE";
+    public static final String GPS_DISABLED = "GPS_DISABLE";
+    public static final String GPS_LIVE_DATA = "GPS_LIVE_DATA";
+    public static final String GPS_PUT_EXTRA = "coordinates";
+    public static final String DATA_LOG_PATH = "storage/emulated/0/Driverify/Logs/";
+    public static final String TIME_INTERVAL_KEY = "obd_update_period_preference";
 
-    public static final int saveSeconds = 10;
-    public static final String obdDataCSVHeader = "rpm speed coolant load fuel_level timestamp";
+    public static final int SAVE_SECONDS = 10;
+    public static final String OBD_DATA_HEADER_CSV = "rpm speed coolant load fuel_level timestamp";
 }
