66b47bec4f1eefff8555e86adfac7a4587ca7d87
Teodor Hristov <26335078+teodtedo771@users.noreply.github.com>
2020-02-21 16:30:21 +0200

    Remove spelling mistake, and change location of java string brackets

--- app/src/main/java/com/demotxt/droidsrce/homedashboard/Home.java
+++ app/src/main/java/com/demotxt/droidsrce/homedashboard/Home.java
@@ -3,25 +3,21 @@ package com.demotxt.droidsrce.homedashboard;
 import android.Manifest;
 import android.content.Context;
 import android.content.Intent;
-
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.pm.PackageManager;
 import android.os.Bundle;
-
-import androidx.cardview.widget.CardView;
-import androidx.core.app.ActivityCompat;
-import androidx.core.content.ContextCompat;
-
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.LinearLayout;
 
-import java.security.Permission;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
+import androidx.core.app.ActivityCompat;
 
 
 public class Home extends AppCompatActivity {
     public static final int PERMISSION_ALL = 1;
-    public static String permissions[] = {
+    public static String[] permissions = {
             Manifest.permission.WRITE_EXTERNAL_STORAGE,
             Manifest.permission.READ_EXTERNAL_STORAGE,
             Manifest.permission.GET_ACCOUNTS,
@@ -31,7 +27,7 @@ public class Home extends AppCompatActivity {
             Manifest.permission.ACCESS_FINE_LOCATION,
             Manifest.permission.ACCESS_COARSE_LOCATION};
     CardView troubleView, driveView;
-    Intent drive, trubleCodes;
+    Intent drive, troubleCodes;
     LinearLayout ll;
 
     @Override
@@ -56,11 +52,11 @@ public class Home extends AppCompatActivity {
             }
         });
 
-        trubleCodes = new Intent(this, TroubleCodes.class);
+        troubleCodes = new Intent(this, TroubleCodes.class);
         troubleView.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                startActivity(trubleCodes);
+                startActivity(troubleCodes);
             }
         });
     }
