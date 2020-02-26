ed99924bfb105db04d45c432aee6efce76206218
Teodor Hristov <26335078+teodtedo771@users.noreply.github.com>
2020-02-26 15:31:59 +0200

    Add constants for face data

--- app/src/main/java/com/demotxt/droidsrce/homedashboard/Utils/Constants.java
+++ app/src/main/java/com/demotxt/droidsrce/homedashboard/Utils/Constants.java
@@ -11,6 +11,7 @@ public class Constants {
     public static final String GPS_LIVE_DATA = "GPS_LIVE_DATA";
     public static final String GPS_PUT_EXTRA = "coordinates";
     public static final String DATA_LOG_PATH = "storage/emulated/0/Driverify/Logs/";
+    public static final String FACE_DATA_PATH = "storage/emulated/0/Driverify/Logs/Face/";
     public static final String TIME_INTERVAL_KEY = "obd_update_period_preference";
     public static final int SAVE_SECONDS = 10;
     public static final String OBD_DATA_HEADER_CSV = "id rpm speed coolant load timestamp";
