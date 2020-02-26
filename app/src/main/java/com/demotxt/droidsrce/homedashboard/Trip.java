762e8d0382e76dd3c705bcdda5157b0885c28451
Teodor Hristov <26335078+teodtedo771@users.noreply.github.com>
2020-02-26 14:15:07 +0200

    Add new trip activity which holds recycler view

---
+++ app/src/main/java/com/demotxt/droidsrce/homedashboard/Trip.java
@@ -0,0 +1,47 @@
+package com.demotxt.droidsrce.homedashboard;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.demotxt.droidsrce.homedashboard.Utils.Constants;
+import com.demotxt.droidsrce.homedashboard.display.ListViewTripDisplayAdapter;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Trip extends AppCompatActivity {
+
+    private ListViewTripDisplayAdapter mAdapter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_trip);
+
+        setFileDataAdapter();
+        setupRecyclerView();
+    }
+
+    private void setFileDataAdapter() {
+        File directory = new File(Constants.DATA_LOG_PATH);
+        List<File> files = new ArrayList<>();
+        for (File file : directory.listFiles()) {
+            if (file.isFile()) {
+                files.add(file);
+            }
+        }
+
+        mAdapter = new ListViewTripDisplayAdapter(getApplicationContext(), files);
+    }
+
+    private void setupRecyclerView() {
+        RecyclerView recyclerView = findViewById(R.id.recyclerView);
+
+        recyclerView.setLayoutManager(new LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false));
+        recyclerView.setAdapter(mAdapter);
+    }
+}
