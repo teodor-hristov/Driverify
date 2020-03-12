8365f0e3ae1b31cbe76c90a3073abb5823120156
Teodor Hristov <26335078+teodtedo771@users.noreply.github.com>
2020-03-12 13:39:22 +0200

    Add face data into DataSynchronizer

--- app/src/main/java/com/demotxt/droidsrce/homedashboard/io/DataSynchronizer.java
+++ app/src/main/java/com/demotxt/droidsrce/homedashboard/io/DataSynchronizer.java
@@ -1,13 +1,19 @@
 package com.demotxt.droidsrce.homedashboard.io;
 
+import java.util.ArrayList;
 import java.util.Queue;
 
 public class DataSynchronizer {
-    private Queue<String> locationQueue, obdDataQueue;
+    private Queue<String> locationQueue, obdDataQueue, faceDataQueue;
 
-    public DataSynchronizer(Queue<String> locationList, Queue<String> obdDataList) {
-        this.locationQueue = locationList;
-        this.obdDataQueue = obdDataList;
+    public DataSynchronizer(Queue<String> locationQueue, Queue<String> obdDataQueue, Queue<String> faceDataQueue) {
+        this.locationQueue = locationQueue;
+        this.obdDataQueue = obdDataQueue;
+        this.faceDataQueue = faceDataQueue;
+    }
+
+    public int Length() {
+        return (locationQueue.size() + obdDataQueue.size() + faceDataQueue.size()) / 3;
     }
 
     public void appendLocation(String locationData) {
@@ -26,43 +32,84 @@ public class DataSynchronizer {
         obdDataQueue.add(obdData);
     }
 
-    //rpm speed coolant load latitude longitude altitude timestamp
-    public String getSynchronizedData() {
-        StringBuilder builder = null;
-        int obdLen;
-        int locationLen;
-        long obdTimestamp;
-        long locationTimestamp;
-
-        for (String obdLine = obdDataQueue.peek(); !obdDataQueue.isEmpty(); obdLine = obdDataQueue.poll()) {
-            obdLen = obdLine.split(" ").length;
-            obdTimestamp = Long.parseLong(obdLine.split(" ")[obdLen - 1]);
-            if (locationQueue.isEmpty()) {
-                builder = new StringBuilder();
-                builder.append(obdLine.split(" ")[0] + " ");
-                builder.append(obdLine.split(" ")[1] + " ");
-                builder.append(obdLine.split(" ")[2] + " ");
-                builder.append(obdLine.split(" ")[3] + " ");
-                builder.append("N/A N/A N/A ");
-                builder.append(obdLine.split(" ")[4] + " ");
-
-            } else {
-                for (String locationLine = locationQueue.peek(); !locationQueue.isEmpty(); locationLine = locationQueue.poll()) {
-                    locationLen = locationLine.split(" ").length;
-                    locationTimestamp = Long.parseLong(locationLine.split(" ")[locationLen - 1]);
-
-                    if (Math.abs(obdTimestamp - locationTimestamp) < 700) {
-                        builder = new StringBuilder();
-                        builder.append(obdLine.split(" ")[0] + " ");
-                        builder.append(obdLine.split(" ")[1] + " ");
-                        builder.append(obdLine.split(" ")[2] + " ");
-                        builder.append(obdLine.split(" ")[3] + " ");
-                        builder.append(locationLine);
-                    }
-                }
-            }
+    public void appendFaceData(String faceData) {
+        if (faceDataQueue.size() >= 10) {
+            faceDataQueue.clear();
         }
 
-        return builder.toString();
+        faceDataQueue.add(faceData);
+    }
+
+    //rpm speed coolant load latitude longitude altitude sleep happiness timestamp
+    public ArrayList<String> getSynchronizedData() {
+        StringBuilder builder = new StringBuilder();
+        ArrayList arrayList = new ArrayList<String>();
+        String[] obdLine = null;
+        String[] locationLine = null;
+        String[] faceLine = null;
+        String obdPart = null, locPart = null, facePart = null;
+
+        while (true) {
+            if (obdDataQueue.isEmpty() && locationQueue.isEmpty() && faceDataQueue.isEmpty()) {
+                break;
+            }
+            if (!obdDataQueue.isEmpty()) {
+                obdLine = obdDataQueue.poll().split(" ");
+            } else {
+                obdLine = new String[]{"N/A", "N/A", "N/A", "N/A", "0"};
+            }
+            if (!locationQueue.isEmpty()) {
+                locationLine = locationQueue.poll().split(" ");
+            } else {
+                locationLine = new String[]{"N/A", "N/A", "N/A", "0"};
+            }
+            if (!faceDataQueue.isEmpty()) {
+                faceLine = faceDataQueue.poll().split(" ");
+            } else {
+                faceLine = new String[]{"N/A", "N/A", "0"};
+            }
+
+            long obdTime = Long.parseLong(obdLine[4]);
+            long locTime = Long.parseLong(locationLine[3]);
+            long faceTime = Long.parseLong(faceLine[2]);
+            long obdLoc = Math.abs(obdTime - locTime);
+            long obdFace = Math.abs(obdTime - faceTime);
+
+
+            if (obdLoc <= 1000) {
+                obdPart = obdLine[0] + " " + obdLine[1] + " " + obdLine[2] + " " + obdLine[3] + " ";
+                locPart = locationLine[0] + " " + locationLine[1] + " " + locationLine[2] + " ";
+                if (obdFace <= 1000) {
+                    facePart = faceLine[0] + " " + faceLine[1] + " " + faceLine[2];
+                } else {
+                    facePart = "N/A N/A " + obdTime;
+                }
+            } else {
+                if (obdTime == 0) {
+                    obdPart = "N/A " + "N/A " + "N/A " + "N/A ";
+                } else {
+                    obdPart = obdLine[0] + " " + obdLine[1] + " " + obdLine[2] + " " + obdLine[3] + " ";
+                }
+                if (locTime == 0) {
+                    locPart = "N/A " + "N/A " + "N/A ";
+                } else {
+                    locPart = locationLine[0] + " " + locationLine[1] + " " + locationLine[2] + " ";
+                }
+                if (faceTime == 0) {
+                    facePart = "N/A " + "N/A " + obdTime;
+                } else {
+                    facePart = faceLine[0] + " " + faceLine[1] + " " + faceLine[2];
+                }
+
+            }
+            if (!faceLine[2].equals("0")) {
+                builder.append(obdPart + locPart + facePart);
+                arrayList.add(builder.toString());
+                builder = new StringBuilder();
+            }
+
+        }
+
+        return arrayList;
     }
 }
