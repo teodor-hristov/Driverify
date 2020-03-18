d9518fd9549b9ba4214ad9e77d13c37c13415dbe
Teodor Hristov <26335078+teodtedo771@users.noreply.github.com>
2020-03-18 13:49:54 +0200

    NightModeSleepDetector is now IntentService because this service need to run in worker thread

--- app/src/main/java/com/demotxt/droidsrce/homedashboard/services/NightModeSleepDetector.java
+++ app/src/main/java/com/demotxt/droidsrce/homedashboard/services/NightModeSleepDetector.java
@@ -1,35 +1,66 @@
 package com.demotxt.droidsrce.homedashboard.services;
 
-import android.app.Service;
+import android.app.IntentService;
+import android.content.BroadcastReceiver;
+import android.content.Context;
 import android.content.Intent;
+import android.content.IntentFilter;
 import android.os.IBinder;
+import android.util.Log;
 
 import androidx.annotation.Nullable;
 
 import com.demotxt.droidsrce.homedashboard.Utils.Constants;
 import com.demotxt.droidsrce.homedashboard.Utils.Methods;
 
-public class NightModeSleepDetector extends Service {
+public class NightModeSleepDetector extends IntentService {
+    private static String TAG = "Test";
+    private BroadcastReceiver receiver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            Log.i(TAG, "rec ok");
+            String action = intent.getAction();
+            long stopTimestamp;
+            switch (action) {
+                case "click":
+                    stopTimestamp = intent.getLongExtra("clickTime", 0);
+                    timeChecker(stopTimestamp, Constants.STARTING_TIME_INTERVAL_NIGHT_DRIVE);
+                    Log.i(TAG, "onReceive: night mode");
+                    break;
+            }
+        }
+    };
+
     @Nullable
     @Override
     public IBinder onBind(Intent intent) {
         return null;
     }
 
+    public NightModeSleepDetector() {
+        super(NightModeSleepDetector.class.getName());
+    }
+
     @Override
-    public int onStartCommand(Intent intent, int flags, int startId) {
-        long currentTime;
+    protected void onHandleIntent(@Nullable Intent intent) {
+        Log.i(TAG, NightModeSleepDetector.class.getSimpleName() + " started...");
+        Log.i(TAG, "Thread id: " + Thread.currentThread().getId());
+        registerReceiver(receiver, new IntentFilter("click"));
         long startTime = System.currentTimeMillis();
         int timeInterval = Constants.STARTING_TIME_INTERVAL_NIGHT_DRIVE;
 
+        timeChecker(startTime, timeInterval);
+    }
+
+    private void timeChecker(long startTime, int timeInterval) {
+        long currentTime;
+
         while (true) {
             currentTime = System.currentTimeMillis();
-            if (startTime - currentTime == Methods.secondsToMillis(timeInterval)) {
-                //TODO: OnClick event. Increase/Decrease timeInterval and set visibitlity for sleepPrevention view
+            if (Math.abs(startTime - currentTime) >= Methods.secondsToMillis(timeInterval)) {
+                startSleepPrevention();
             }
         }
-
-        return super.onStartCommand(intent, flags, startId);
     }
 
     @Override
