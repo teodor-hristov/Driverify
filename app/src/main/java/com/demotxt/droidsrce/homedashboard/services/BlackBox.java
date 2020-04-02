095ac820d8c4754b2719ee4cedfb75acbd547a2a
Teodor Hristov <26335078+teodtedo771@users.noreply.github.com>
2020-04-02 18:09:42 +0300

    Update BlackBox service. This service is now used only like a redirection service.

--- app/src/main/java/com/demotxt/droidsrce/homedashboard/services/BlackBox.java
+++ app/src/main/java/com/demotxt/droidsrce/homedashboard/services/BlackBox.java
@@ -1,38 +1,44 @@
 package com.demotxt.droidsrce.homedashboard.services;
 
-import android.Manifest;
-import android.app.Activity;
 import android.app.IntentService;
 import android.content.Intent;
-import android.content.pm.PackageManager;
+import android.util.Log;
 
-import androidx.core.app.ActivityCompat;
+import com.demotxt.droidsrce.homedashboard.Utils.Constants;
+import com.demotxt.droidsrce.homedashboard.io.ZipManager;
+
+import java.io.File;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class BlackBox extends IntentService {
-    private static String[] PERMISSIONS_STORAGE = {
-            Manifest.permission.WRITE_EXTERNAL_STORAGE,
-            Manifest.permission.READ_EXTERNAL_STORAGE
-    };
-    private static int REQUEST_EXTERNAL_STORAGE = 0;
+    private final String TAG = "BlackBox";
 
     public BlackBox() {
         super("BlackBox");
     }
 
-    public static void verifyStoragePermissions(Activity activity) {
-        int permission = ActivityCompat.checkSelfPermission(activity, Manifest.permission.WRITE_EXTERNAL_STORAGE);
-
-        if (permission != PackageManager.PERMISSION_GRANTED) {
-            ActivityCompat.requestPermissions(
-                    activity,
-                    PERMISSIONS_STORAGE,
-                    REQUEST_EXTERNAL_STORAGE
-            );
-        }
-    }
-
     @Override
     protected void onHandleIntent(Intent intent) {
+        Log.i(TAG, BlackBox.class.getSimpleName() + " started...");
+        Log.i(TAG, "Thread id: " + Thread.currentThread().getId());
+
+        ZipManager zipManager = new ZipManager();
+        File logs = new File(Constants.DATA_LOG_PATH);
+        File blackBoxDir = new File(Constants.BLACK_BOX_DIRECTORY);
+        String[] logFiles;
+
+        logFiles = logs.list();
+        int len = logFiles.length;
+        for (int i = 0; i < len; i++) {
+            logFiles[i] = Constants.DATA_LOG_PATH + logFiles[i];
+        }
+
+        if (!blackBoxDir.exists()) {
+            blackBoxDir.mkdirs();
+        }
+
+        zipManager.zip(logFiles, Constants.BLACK_BOX_DIRECTORY + new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss").format(new Date()) + ".zip");
 
     }
 
