117c5ff1da0229e32c1fa9f47af59d5158b90e5b
Teodor Hristov <26335078+teodtedo771@users.noreply.github.com>
2020-04-02 18:07:38 +0300

    Add constant BLACK_BOX_DIRECTORY and remove redundant constants FACE_DATA_PATH; LOCATION_HEADER_CSV; FACE_DATA_HEADER

--- app/src/main/java/com/demotxt/droidsrce/homedashboard/Utils/Constants.java
+++ app/src/main/java/com/demotxt/droidsrce/homedashboard/Utils/Constants.java
@@ -11,15 +11,13 @@ public class Constants {
     public static final String GPS_LIVE_DATA = "GPS_LIVE_DATA";
     public static final String GPS_PUT_EXTRA = "coordinates";
     public static final String DATA_LOG_PATH = "storage/emulated/0/Driverify/Logs/";
+    public static final String BLACK_BOX_DIRECTORY = "storage/emulated/0/Driverify/Black box/";
     public static final String LOCATON_LIVE_DATA_PATH = "storage/emulated/0/Driverify/Logs/Location/";
-    public static final String FACE_DATA_PATH = "storage/emulated/0/Driverify/Logs/Face/";
     public static final String TIME_INTERVAL_KEY = "obd_update_period_preference";
     public static final int SAVE_SECONDS = 10;
     public static final String UNITED_DATA_HEADER = "rpm speed coolant load latitude longitude altitude sleep happiness timestamp";
-    public static final String LOCATION_HEADER_CSV = "latitude longitude altitude timestamp";
     public static final String CHECKOUT_TRIP = "checkout_trip";
     public static final String FACE_DATA = "FACE_DATA";
-    public static final String FACE_DATA_HEADER = "sleep happiness timestamp";
     public static final int MAX_RPM = 7000;
     public static final int MAX_COOLANT = 180;
     public static final int MAX_SPEED = 260;
