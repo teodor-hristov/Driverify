9e6e3a006dab29f593f927a5eea1cdc505db01d0
Teodor Hristov <26335078+teodtedo771@users.noreply.github.com>
2020-04-03 16:23:52 +0300

    Add method calculateDistanceBetweenTwoPoints in MapTripFragment

--- app/src/main/java/com/demotxt/droidsrce/homedashboard/ui/main/MapTripFragment.java
+++ app/src/main/java/com/demotxt/droidsrce/homedashboard/ui/main/MapTripFragment.java
@@ -208,5 +208,28 @@ public class MapTripFragment extends Fragment implements OnMapReadyCallback {
                     .snippet("speed: " + speed + " km/h rpm: " + rpm + " load: " + load + "%"));
         }
     }
+
+    public double calculateDistanceBetweenTwoPoints(LatLng StartP, LatLng EndP) {
+        int Radius = 6371;// radius of earth in Km
+        double lat1 = StartP.latitude;
+        double lat2 = EndP.latitude;
+        double lon1 = StartP.longitude;
+        double lon2 = EndP.longitude;
+        double dLat = Math.toRadians(lat2 - lat1);
+        double dLon = Math.toRadians(lon2 - lon1);
+        double a = Math.sin(dLat / 2) * Math.sin(dLat / 2)
+                + Math.cos(Math.toRadians(lat1))
+                * Math.cos(Math.toRadians(lat2)) * Math.sin(dLon / 2)
+                * Math.sin(dLon / 2);
+        double c = 2 * Math.asin(Math.sqrt(a));
+        double valueResult = Radius * c;
+        double km = valueResult / 1;
+        DecimalFormat newFormat = new DecimalFormat("####");
+        int kmInDec = Integer.valueOf(newFormat.format(km));
+        double meter = valueResult % 1000;
+        int meterInDec = Integer.valueOf(newFormat.format(meter));
+
+        return Radius * c;
+    }
 }
 
